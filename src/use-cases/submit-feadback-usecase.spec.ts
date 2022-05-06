57a33521070820ec0ef1a4e0321728d6453722c2
Junior Paula <slucivaldo509@gmail.com>
2022-05-06 08:42:43 -0300

    test: ensure not be able to submita feadback without type

--- src/use-cases/submit-feadback-usecase.spec.ts
+++ src/use-cases/submit-feadback-usecase.spec.ts
@@ -13,4 +13,17 @@ describe('SubmitFeadback', () => {
       screenshot: 'data:image/png;base64,any_screenshot'
     })).resolves.not.toThrow()
   })
+
+  test('Should not be able to submit a feadback without type', async () => {
+    const submitFeadback = new SubmitFeadbackUsecase(
+      { create: async () => {} },
+      { sendMail: async () => {} }
+    )
+
+    expect(submitFeadback.handle({
+      type: '',
+      comment: 'any_comment',
+      screenshot: 'data:image/png;base64,any_screenshot'
+    })).rejects.toThrow()
+  })
 })
