28875fe8a24384df2a112801e316595c64764757
Junior Paula <slucivaldo509@gmail.com>
2022-05-06 05:36:41 -0300

    feat: add feadback route

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,22 @@
+import { prisma } from './prisma'
 import express from 'express'
 
 const app = express()
+
+app.use(express.json())
+
+app.post('/feadback', async (req, res) => {
+  const { type, comment, screenshot } = req.body
+
+  const feadback = await prisma.feadback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  })
+
+  return res.status(201).json(feadback)
+})
 
 app.listen(3030, () => console.log('[+] Server is running'))
