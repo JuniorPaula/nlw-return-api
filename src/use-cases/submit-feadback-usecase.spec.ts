b2caead718b7dedd840e21ec64306f90ccfb79fb
Junior Paula <slucivaldo509@gmail.com>
2022-05-06 08:40:20 -0300

    test: ensure be able to submita feadback

---
+++ src/use-cases/submit-feadback-usecase.spec.ts
@@ -0,0 +1,16 @@
+import { SubmitFeadbackUsecase } from './submit-feadback-usercase'
+
+describe('SubmitFeadback', () => {
+  test('Should be able to submit a feadback', async () => {
+    const submitFeadback = new SubmitFeadbackUsecase(
+      { create: async () => {} },
+      { sendMail: async () => {} }
+    )
+
+    expect(submitFeadback.handle({
+      type: 'any_type',
+      comment: 'any_comment',
+      screenshot: 'data:image/png;base64,any_screenshot'
+    })).resolves.not.toThrow()
+  })
+})
