6eec07cc52f51273d364836da8dc122e655c9cb6
Junior Paula <slucivaldo509@gmail.com>
2022-05-06 08:40:11 -0300

    feat: ensure be able to submita feadback

--- src/use-cases/submit-feadback-usercase.ts
+++ src/use-cases/submit-feadback-usercase.ts
@@ -21,6 +21,18 @@ export class SubmitFeadbackUsecase {
   async handle (request: SubmitFeadbackUsecaseRequest): Promise<void> {
     const { type, comment, screenshot } = request
 
+    if (!type) {
+      throw new Error('Type is required!')
+    }
+
+    if (!comment) {
+      throw new Error('Comment is required!')
+    }
+
+    if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
+      throw new Error('Invalid screenshot format!')
+    }
+
     await this.feadbackRepository.create({
       type,
       comment,
