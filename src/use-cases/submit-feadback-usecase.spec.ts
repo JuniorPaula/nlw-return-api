26bb14572acf2958d6e40ac92499b3f7408de3e3
Junior Paula <slucivaldo509@gmail.com>
2022-05-07 08:03:56 -0300

    refactor: move sut to a factory method

--- src/use-cases/submit-feadback-usecase.spec.ts
+++ src/use-cases/submit-feadback-usecase.spec.ts
@@ -1,13 +1,61 @@
+import { FeadbacksRepository, FeadbacksRepositoryDTO } from '../repositories/interfaces/feadbacks-repository'
+import { SendEmail, SendEmailDTO } from '../service/interfaces/send-email'
 import { SubmitFeadbackUsecase } from './submit-feadback-usercase'
+
+const makeFeadbackRepository = (): FeadbacksRepository => {
+  class FeadbackRepositoryStub implements FeadbacksRepository {
+    async create (data: FeadbacksRepositoryDTO): Promise<void> {
+      const fakeFeadback = {
+        type: 'any_type',
+        comment: 'any_comment',
+        screenshot: 'data:image/png;base64,any_screenshot'
+      }
+
+      await new Promise(resolve => resolve(fakeFeadback))
+    }
+  }
+
+  return new FeadbackRepositoryStub()
+}
+
+const makeSendEmail = (): SendEmail => {
+  class SendEmailStub implements SendEmail {
+    async sendMail (data: SendEmailDTO): Promise<void> {
+      const fakeMailSend = {
+        subject: 'any_feadback',
+        body: 'any_body'
+      }
+
+      await new Promise(resolve => resolve(fakeMailSend))
+    }
+  }
+
+  return new SendEmailStub()
+}
+
+interface SutTypes {
+  sut: SubmitFeadbackUsecase
+  feadbackRepositoryStub: FeadbacksRepository
+  sendEmailStub: SendEmail
+}
+
+const makeSut = (): SutTypes => {
+  const feadbackRepositoryStub = makeFeadbackRepository()
+  const sendEmailStub = makeSendEmail()
+  const sut = new SubmitFeadbackUsecase(feadbackRepositoryStub, sendEmailStub)
+
+  return {
+    sut,
+    feadbackRepositoryStub,
+    sendEmailStub
+  }
+}
 
 describe('SubmitFeadback', () => {
   test('Should be able to submit a feadback', async () => {
-    const submitFeadback = new SubmitFeadbackUsecase(
-      { create: async () => {} },
-      { sendMail: async () => {} }
-    )
+    const { sut } = makeSut()
 
-    expect(submitFeadback.handle({
+    expect(sut.handle({
       type: 'any_type',
       comment: 'any_comment',
       screenshot: 'data:image/png;base64,any_screenshot'
@@ -15,12 +63,9 @@ describe('SubmitFeadback', () => {
   })
 
   test('Should not be able to submit a feadback without type', async () => {
-    const submitFeadback = new SubmitFeadbackUsecase(
-      { create: async () => {} },
-      { sendMail: async () => {} }
-    )
+    const { sut } = makeSut()
 
-    expect(submitFeadback.handle({
+    expect(sut.handle({
       type: '',
       comment: 'any_comment',
       screenshot: 'data:image/png;base64,any_screenshot'
@@ -28,12 +73,9 @@ describe('SubmitFeadback', () => {
   })
 
   test('Should not be able to submit a feadback without comment', async () => {
-    const submitFeadback = new SubmitFeadbackUsecase(
-      { create: async () => {} },
-      { sendMail: async () => {} }
-    )
+    const { sut } = makeSut()
 
-    expect(submitFeadback.handle({
+    expect(sut.handle({
       type: 'any_type',
       comment: '',
       screenshot: 'data:image/png;base64,any_screenshot'
@@ -41,12 +83,9 @@ describe('SubmitFeadback', () => {
   })
 
   test('Should not be able to submit a feadback with invalid screenshot', async () => {
-    const submitFeadback = new SubmitFeadbackUsecase(
-      { create: async () => {} },
-      { sendMail: async () => {} }
-    )
+    const { sut } = makeSut()
 
-    expect(submitFeadback.handle({
+    expect(sut.handle({
       type: 'any_type',
       comment: '',
       screenshot: 'invalid_screenshot'
